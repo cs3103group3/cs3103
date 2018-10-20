f994ea60d5bd6a4fd4c17327853f855a4780fc01
DESKTOP-I8I0R59\limweijie <limweijie1994@gmail.com>
2018-10-20 18:06:33 +0800

    Updates

---
+++ src/main/tracker/Record.java
@@ -0,0 +1,24 @@
+package main.tracker;
+
+/**
+ * This class keeps all the information regarding one chunk
+ * @author limwe
+ *
+ */
+public class Record {
+	
+	String ipAddress;
+	String chunkNumber;
+	
+	public Record(String ipAdd, String chunkNo) {
+		this.ipAddress = ipAdd;
+		this.chunkNumber = chunkNo;
+	}
+	
+	public String getipAdd() {
+		return this.ipAddress;
+	}
+	public String getChunkNo() {
+		return this.chunkNumber;
+	}
+}
