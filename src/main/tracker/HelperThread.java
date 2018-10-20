a7dde30d08865878ca208e5d9193418d7105be80
DESKTOP-I8I0R59\limweijie <limweijie1994@gmail.com>
2018-10-20 16:55:22 +0800

    Commit Descrpiption and format for accepted commands from User

--- src/main/tracker/HelperThread.java
+++ src/main/tracker/HelperThread.java
@@ -51,32 +51,43 @@ public class HelperThread extends Thread{
 	 */
 	private void doClientCommand(String strCommand) {
 	    InterfaceCommand command = InterfaceCommand.INVALID;
+	    String [] strCommandArr;
 	    try {
-	        int commandCode = Integer.parseInt(strCommand);
+	    	strCommand = strCommand.trim();
+	    	strCommandArr = strCommand.split(" ");
+	        int commandCode = Integer.parseInt(strCommandArr[0]);
 	        command = InterfaceCommand.forCode(commandCode);
 	    } catch(NumberFormatException nfe) {
 	        reply.println(ErrorMessage.INVALID_COMMAND.getErrorMessage());
 	        return;
 	    }
+	   
 	    
+	    //Calls method that is required to execute the user's command
 		switch(command) {
 		case LIST:
 			//perform list
+			listDirectoryEntry();
 			break;
 		case CHANGE_DIRECTORY:
             //Change directory
+			changeDirectory(strCommandArr);
             break;
 		case SEARCH:
 			//perform search
+			searchEntry(strCommandArr);
 			break;
 		case DOWNLOAD:
-			//perform downloading of file
+			//Finds peer to download the file requested
+			findPeer(strCommandArr);
 			break;
 		case INFORM:
-            //perform downloading of file
-            break;
+            //Update the server of newly advertised chunk of file
+            informServer(strCommandArr);
+			break;
 		case QUIT:
 			//perform exit
+			exitServer();
 			break;
 		default:
 			//Error
@@ -84,4 +95,113 @@ public class HelperThread extends Thread{
 			return;
 		}
 	}
+	
+
+	/**
+	 * Lists the directory Entry
+	 * 
+	 * Example Input:
+	 * 1
+	 * Expected Output:
+	 * Directory Entry
+	 */
+	private synchronized void listDirectoryEntry() {
+		
+	}
+	
+	/**
+	 * Changes the directory entry
+	 * @param strCommandArr: the Array of command by client that has been split
+	 * 
+	 * Example Input:
+	 * 2 /FolderTwo
+	 * Expected Output:
+	 * Directory Changed to '/FolderTwo'
+	 * Then calls listDirectoryEntry again
+	 */
+	private synchronized void changeDirectory(String[] strCommandArr) {
+		
+		
+	}
+	
+	
+	/**
+	 * Search Entry by the following
+	 * @param strCommandArr:  the Array of command by client that has been split
+	 * 1) FileName
+	 * 2) Chunk Number
+	 * 
+	 * Example Input One:
+	 * 3 fileNameOne
+	 * Expected Output:
+	 * HostName/IP, FileName to be written back to the peer who requested it
+	 * 
+	 * Example Input Two:
+	 * 3 chunkOne
+	 * Expected Output:
+	 * HostName/IP, FileName to be written back to the peer who requested it
+	 * 
+	 * Invalid Output:
+	 * "Invalid fileName or Chunk Number specified"
+	 */
+	private synchronized void searchEntry(String[] strCommandArr) {
+		
+	}
+	
+	/**
+	 * Informs the Server of the new chunk to be advertised
+	 * @param strCommandArr
+	 * 
+	 * Format of Input:
+	 * 4 hostname/IP FileName chunkNumber
+	 * Example Input:
+	 * 4 192.168.1.192 file.txt 5
+	 * 
+	 * Notes:
+	 * 1) Might need to check if IP is valid
+	 * 2) Might need to check if fileName is valid
+	 */
+	private synchronized void informServer(String[] strCommandArr) {
+		
+		
+	}
+	
+	/**
+	 * Asks the server for location of file or chunk from a peer
+	 * @param strCommandArr
+	 * 
+	 * Format of Input:
+	 * 5 fileName/chunkNumber
+	 * 
+	 * Example Input:
+	 * 5 example.txt
+	 * Expected Output:
+	 * IP address is output to user
+	 * Invalid Output:
+	 * Unable to find specific file or chunk
+	 * 
+	 */
+	private synchronized void findPeer(String[] strCommandArr) {
+		
+		
+	}
+	
+	/**
+	 * Exits the server
+	 * 
+	 * Example Input:
+	 * 6
+	 * 
+	 * Expected Output:
+	 * You have exited the server
+	 * 
+	 * Note:
+	 * 1) Have to delete the fileName/chunk listed in the central server
+	 */
+	private void exitServer() {
+		
+		
+	}
+
+
 }
