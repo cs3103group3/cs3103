c14a804716081560cd3341eada0aa057a86b42d9
marcusngwj <marcusngwj@msn.com>
2018-10-20 10:11:07 +0800

    update message

--- src/message/Message.java
+++ src/message/Message.java
@@ -1,6 +1,20 @@
 package message;
 
+
 public class Message {
-    int type;
-    byte[] data;
+    private MessageType type;
+    private byte[] data;
+    
+    public Message(MessageType type, byte[] data) {
+        this.type = type;
+        this.data = data;
+    }
+    
+    public MessageType getMessageType() {
+        return this.type;
+    }
+    
+    public byte[] getData() {
+        return this.data;
+    }
 }
