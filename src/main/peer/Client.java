8decd7d37bbc4638c4916259be1634d9ad3b6223
Brehmer Chan <brehmerchan@gmail.com>
2018-10-24 11:23:33 +0800

    add client.quit()

--- src/main/peer/Client.java
+++ src/main/peer/Client.java
@@ -117,7 +117,17 @@ public class Client extends Thread {
         
     }
     
-    private void quit() {
+    private void quit() throws UnknownHostException, IOException {
+    	//Inform server that it is exiting
+    	Socket clientSocket = new Socket(NetworkConstant.TRACKER_HOSTNAME, NetworkConstant.TRACKER_LISTENING_PORT);
+ 
+		PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+		out.println(InterfaceCommand.QUIT.getCommandCode());
+		out.flush();
+		 
+		clientSocket.close();
+         
+		//TODO: close server sockets
         System.out.println("Goodbye!");
     }
         
