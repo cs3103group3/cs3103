86112fe3eff1038275da9f0cdf180dc3e28e79a0
Mamtha <mamtha3005@gmail.com>
2018-10-24 10:10:28 +0800

    Comment message stuff - Not required

--- src/main/message/MessageType.java
+++ src/main/message/MessageType.java
@@ -1,11 +1,11 @@
-package main.message;
-
-public enum MessageType {
-    CONNECTION_REQUEST,
-    CONNECTION_REPLY, 
-    DIRECTORY_LISTING_REQUEST,
-    DIRECTORY_LISTING_REPLY,
-    DATA_CHUNK_REQUEST,
-    DATA_CHUNK_REPLY,
-    LEAVE_NETWORK;
-}
+//package main.message;
+//
+//public enum MessageType {
+//    CONNECTION_REQUEST,
+//    CONNECTION_REPLY, 
+//    DIRECTORY_LISTING_REQUEST,
+//    DIRECTORY_LISTING_REPLY,
+//    DATA_CHUNK_REQUEST,
+//    DATA_CHUNK_REPLY,
+//    LEAVE_NETWORK;
+//}
