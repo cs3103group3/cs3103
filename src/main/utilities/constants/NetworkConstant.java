7bfb9491b8146271c81cb984cad7dcc9ff49a915
Brehmer Chan <brehmerchan@gmail.com>
2018-11-02 20:26:04 +0800

    change IP to digital ocean IP

--- src/main/utilities/constants/NetworkConstant.java
+++ src/main/utilities/constants/NetworkConstant.java
@@ -5,7 +5,7 @@ public final class NetworkConstant {
     
     // Tracker
     public static final int TRACKER_LISTENING_PORT = 7878;
-    public static final String TRACKER_HOSTNAME = "192.168.56.1";
+    public static final String TRACKER_HOSTNAME = "68.183.121.174";
     
     // Peer Client
     public static final int CLIENT_LISTENING_PORT = 2121;
